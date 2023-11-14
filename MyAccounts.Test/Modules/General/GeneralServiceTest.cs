ffa6faeb966f25e52cfc4bc743d3083fb6fb9a22
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2023-11-13 23:00:31 -0500

    Add GeneralServiceTest

---
+++ MyAccounts.Test/Modules/General/GeneralServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using MyAccounts.Api.Modules.General;
+
+namespace MyAccounts.Test.Modules.General
+{
+    public class GeneralServiceTest
+    {
+        private readonly FakeMyAccountsDb _db = new();
+        private readonly GeneralService _service;
+
+        public GeneralServiceTest()
+        {
+            var contextMock = MockHelper.GetMyAccountsContextMock(_db);
+            var dtoserviceMock = MockHelper.GetDtoServiceMock();
+
+            _service = new GeneralService(contextMock.Object, dtoserviceMock.Object);
+        }
+    }
+}
